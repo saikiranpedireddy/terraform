7fd2333d1c72352547bb6894a1427be7aacc7c38
Pedireddy Saikiran <71814347+saikiranpedireddy@users.noreply.github.com>
2021-03-19 15:42:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region = "ap-south-1"
-  access_key : "AKIAXHEUMTELSMNXBLG2"
-  secret_key : "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"
+  region  = "${var.region}"
+  version = "~> 2.0"
 }
