131fcf98a3042e575fb54a2ca7a2b142ba6485f5
Pedireddy Saikiran <71814347+saikiranpedireddy@users.noreply.github.com>
2021-03-19 15:43:58 +0530

    Update and rename resources.tf to variables.tf

--- resources.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "first-EC2" {
-  ami = "ami-0eeb03e72075b9bcc"
-  instance_type = "t2.micro"
-           tags = {
-                  Name = "terraform-myfirstEC2"
-            }
-}
